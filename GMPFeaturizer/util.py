4c5a2f9f74baba3b706eaa7969a53c8f55c3f991
Ray-Lei <ray.lei@tri.global>
2022-07-12 11:39:06 -0400

    init

---
+++ GMPFeaturizer/util.py
@@ -0,0 +1,68 @@
+import hashlib
+
+import numpy as np
+
+from .constants import ATOM_INDEX_TO_SYMBOL_DICT, ATOM_SYMBOL_TO_INDEX_DICT
+
+# from ase.io.trajectory import Trajectory
+
+
+def _gen_2Darray_for_ffi(arr, ffi, cdata="double"):
+    # Function to generate 2D pointer for cffi
+    shape = arr.shape
+    arr_p = ffi.new(cdata + " *[%d]" % shape[0])
+    for i in range(shape[0]):
+        arr_p[i] = ffi.cast(cdata + " *", arr[i].ctypes.data)
+    return arr_p
+
+
+def get_hash(image, ref_positions):
+
+    string = ""
+    string += str(image.pbc)
+    try:
+        flattened_cell = image.cell.array.flatten()
+    except AttributeError:  # older ASE
+        flattened_cell = image.cell.flatten()
+    for number in flattened_cell:
+        string += "%.15f" % number
+    for number in image.get_atomic_numbers():
+        string += "%3d" % number
+    for number in image.get_positions(wrap=True).flatten():
+        string += "%.15f" % number
+
+    md5_1 = hashlib.md5(string.encode("utf-8"))
+    hash1 = md5_1.hexdigest()
+
+    pos_string = ""
+    for number in ref_positions.flatten():
+        pos_string += "%.15f" % number
+
+    md5_2 = hashlib.md5(pos_string.encode("utf-8"))
+    hash2 = md5_2.hexdigest()
+
+    return "{}-{}".format(hash1, hash2)
+
+
+def validate_image(image, ref_positions):
+    for number in image.get_scaled_positions(wrap=True).flatten():
+        if number > 1.0 or number < 0.0:
+            raise ValueError(
+                "****ERROR: scaled position not strictly between [0, 1]"
+                "Please check atom position and system cell size are set up correctly"
+            )
+    return
+
+
+def list_symbols_to_indices(list_of_symbols):
+    list_indices = []
+    for symbol in list_of_symbols:
+        list_indices.append(ATOM_SYMBOL_TO_INDEX_DICT[symbol])
+    return np.array(list_indices, dtype=np.intc)
+
+
+def list_indices_to_symbols(list_of_indices):
+    list_symbols = []
+    for index in list_of_indices:
+        list_symbols.append(ATOM_INDEX_TO_SYMBOL_DICT[index])
+    return list_symbols
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup_requires = [
+    "cffi>=1.0.0",
+]
+
+install_requires = [
+    "ase",
+    "tqdm",
+    "numpy",
+    "h5py",
+    "cffi",
+]
+
+setup(
+    name="gmp-featurizer",
+    version="0.1",
+    description="Feature calculator for GMP features",
+    author="Xiangyun Lei",
+    author_email="ray.lei@tri.global",
+    url="https://github.com/TRI-AMDD/GMP-featurizer",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    package_data={"": ["*.cpp", "*.h"]},
+    python_requires=">=3.6, <4",
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    cffi_modules=[
+        "GMPFeaturizer/GMPOrderNorm/libgmpordernorm_builder.py:ffibuilder",
+    ],
+)
