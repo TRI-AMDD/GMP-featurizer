cc5abf02de0cf18e45ee8bea993ea7ef482eedbc
Ray-Lei <ray.lei@tri.global>
2022-10-11 11:04:54 -0400

    move parallelization to class method

--- GMPFeaturizer/gmp_featurizer.py
+++ GMPFeaturizer/gmp_featurizer.py
@@ -11,93 +11,93 @@ from .util import get_hash, list_symbols_to_indices#, validate_image
 from .converters import ASEAtomsConverter, PymatgenStructureConverter
 
 
-def calculate_GMP_features(
-    feature_setup,
-    elements,
-    images,
-    ref_positions_list=None,
-    calc_derivatives=False,
-    save_features=False,
-    verbose=False,
-    cores=1,
-):
+# def calculate_GMP_features(
+#     feature_setup,
+#     elements,
+#     images,
+#     ref_positions_list=None,
+#     calc_derivatives=False,
+#     save_features=False,
+#     verbose=False,
+#     cores=1,
+# ):
 
-    # images_feature_list = []
+#     # images_feature_list = []
 
-    if ref_positions_list is None:
-        ref_positions_list = [image.get_positions() for image in images]
+#     if ref_positions_list is None:
+#         ref_positions_list = [image.get_positions() for image in images]
 
-    assert len(images) == len(ref_positions_list)
+#     assert len(images) == len(ref_positions_list)
 
-    # if save is true, create directories if not exist
-    # feature._setup_feature_database(save_features=save_features)
+#     # if save is true, create directories if not exist
+#     # feature._setup_feature_database(save_features=save_features)
 
-    if cores <= 1:
-        feature = GMP(feature_setup, elements)
-        images_feature_list = []
-        for image, ref_positions in tqdm(
-            zip(images, ref_positions_list),
-            total=len(images),
-            desc="Computing features",
-            disable=not verbose,
-        ):
-            temp_image_dict = feature._calculate_single_image(
-                image,
-                ref_positions,
-                calc_derivatives,
-                save_features,
-            )
-            images_feature_list.append(temp_image_dict)
-        return images_feature_list
+#     if cores <= 1:
+#         feature = GMP(feature_setup, elements)
+#         images_feature_list = []
+#         for image, ref_positions in tqdm(
+#             zip(images, ref_positions_list),
+#             total=len(images),
+#             desc="Computing features",
+#             disable=not verbose,
+#         ):
+#             temp_image_dict = feature._calculate_single_image(
+#                 image,
+#                 ref_positions,
+#                 calc_derivatives,
+#                 save_features,
+#             )
+#             images_feature_list.append(temp_image_dict)
+#         return images_feature_list
 
-    elif cores > 1:
+#     elif cores > 1:
 
-        remote_feature_actor = ray.remote(GMP)
-        length = len(images)
-        calc_deriv_list = [calc_derivatives] * length
-        save_features_list = [save_features] * length
-        args = zip(images, ref_positions_list, calc_deriv_list, save_features_list)
+#         remote_feature_actor = ray.remote(GMP)
+#         length = len(images)
+#         calc_deriv_list = [calc_derivatives] * length
+#         save_features_list = [save_features] * length
+#         args = zip(images, ref_positions_list, calc_deriv_list, save_features_list)
 
-        ray.init(num_cpus=cores)
-        actors = [
-            remote_feature_actor.remote(feature_setup, elements) for _ in range(cores)
-        ]
-        pool = ActorPool(actors)
-        poolmap = pool.map(
-            lambda a, v: a._calculate_single_image.remote(v[0], v[1], v[2], v[3]), args
-        )
-        images_feature_list = [a for a in tqdm(poolmap, total=length)]
+#         ray.init(num_cpus=cores)
+#         actors = [
+#             remote_feature_actor.remote(feature_setup, elements) for _ in range(cores)
+#         ]
+#         pool = ActorPool(actors)
+#         poolmap = pool.map(
+#             lambda a, v: a._calculate_single_image.remote(v[0], v[1], v[2], v[3]), args
+#         )
+#         images_feature_list = [a for a in tqdm(poolmap, total=length)]
 
-        ray.shutdown()
-        return images_feature_list
+#         ray.shutdown()
+#         return images_feature_list
 
-        # length = len(images)
-        # obj_ids = [remote_actor.remote(feature, image, ref_positions, calc_derivatives, save_features) for image, ref_positions in zip(images, ref_positions_list)]
+#         # length = len(images)
+#         # obj_ids = [remote_actor.remote(feature, image, ref_positions, calc_derivatives, save_features) for image, ref_positions in zip(images, ref_positions_list)]
 
-        # for x in tqdm(to_iterator(obj_ids), total=length):
-        #     pass
+#         # for x in tqdm(to_iterator(obj_ids), total=length):
+#         #     pass
 
-        # from .util import istarmap
-        # import multiprocessing.pool as mpp
-        # from multiprocessing import Pool
+#         # from .util import istarmap
+#         # import multiprocessing.pool as mpp
+#         # from multiprocessing import Pool
 
-        # mpp.Pool.istarmap = istarmap
+#         # mpp.Pool.istarmap = istarmap
 
-        # length = len(images)
-        # calc_deriv_list = [calc_derivatives] * length
-        # save_features_list = [save_features] * length
-        # args = zip(images, ref_positions_list, calc_deriv_list, save_features_list)
-        # images_feature_list = []
-        # with Pool(cores) as p:
-        #     for temp_image_dict in tqdm(
-        #         p.istarmap(self._calculate_single_image, args), total=length
-        #     ):
-        #         images_feature_list.append(temp_image_dict)
+#         # length = len(images)
+#         # calc_deriv_list = [calc_derivatives] * length
+#         # save_features_list = [save_features] * length
+#         # args = zip(images, ref_positions_list, calc_deriv_list, save_features_list)
+#         # images_feature_list = []
+#         # with Pool(cores) as p:
+#         #     for temp_image_dict in tqdm(
+#         #         p.istarmap(self._calculate_single_image, args), total=length
+#         #     ):
+#         #         images_feature_list.append(temp_image_dict)
 
-        # return images_feature_list
+#         # return images_feature_list
 
-    else:
-        raise ValueError
+#     else:
+#         raise ValueError
 
 
 class GMPFeaturizer:
@@ -129,12 +129,15 @@ class GMPFeaturizer:
         if ref_positions_list == None:
             ref_positions_list = [image["atom_positions"] for image in images]
 
+        assert len(images) == len(ref_positions_list)
+
         # self.calculated_features_list = self.feature.calculate(
-        calculated_features_list = calculate_GMP_features(
-            self.feature_setup,
-            self.elements,
+        # calculated_features_list = calculate_GMP_features(
+        calculated_features_list = self._calculate_GMP_features(
+            # self.feature_setup,
+            # self.elements,
             images,
-            ref_positions_list,
+            ref_positions_list=ref_positions_list,
             calc_derivatives=self.calc_derivatives,
             save_features=self.save_features,
             cores=cores,
@@ -171,7 +174,8 @@ class GMPFeaturizer:
             assert "atom_positions" in image
             assert "atom_symbols" in image
             assert "cell" in image
-            assert "occupancies" in image
+            if "occupancies" not in image:
+                image["occupancies"] = np.array([1.0 for _ in range(len(image["atom_symbols"]))])
             assert len(image["atom_positions"]) == len(image["atom_symbols"])
             assert len(image["atom_positions"]) == len(image["occupancies"])
 
@@ -195,4 +199,63 @@ class GMPFeaturizer:
     # TODO
     def get_stats(self):
         raise NotImplementedError
+
+
+    def _calculate_GMP_features(
+        self,
+        # feature_setup,
+        # elements,
+        images,
+        ref_positions_list=None,
+        calc_derivatives=False,
+        save_features=False,
+        verbose=False,
+        cores=1,
+    ):
+        # if ref_positions_list is None:
+        #     ref_positions_list = [image.get_positions() for image in images]
+
+        # assert len(images) == len(ref_positions_list)
+
+        # if save is true, create directories if not exist
+        # feature._setup_feature_database(save_features=save_features)
+
+        if cores <= 1:
+            feature = GMP(self.feature_setup, self.elements)
+            images_feature_list = []
+            for image, ref_positions in tqdm(
+                zip(images, ref_positions_list),
+                total=len(images),
+                desc="Computing features",
+                disable=not verbose,
+            ):
+                temp_image_dict = feature._calculate_single_image(
+                    image,
+                    ref_positions,
+                    calc_derivatives,
+                    save_features,
+                )
+                images_feature_list.append(temp_image_dict)
+            return images_feature_list
+
+        elif cores > 1:
+
+            remote_feature_actor = ray.remote(GMP)
+            length = len(images)
+            calc_deriv_list = [calc_derivatives] * length
+            save_features_list = [save_features] * length
+            args = zip(images, ref_positions_list, calc_deriv_list, save_features_list)
+
+            ray.init(num_cpus=cores)
+            actors = [
+                remote_feature_actor.remote(self.feature_setup, self.elements) for _ in range(cores)
+            ]
+            pool = ActorPool(actors)
+            poolmap = pool.map(
+                lambda a, v: a._calculate_single_image.remote(v[0], v[1], v[2], v[3]), args
+            )
+            images_feature_list = [a for a in tqdm(poolmap, total=length)]
+
+            ray.shutdown()
+            return images_feature_list
 
